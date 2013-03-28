e8770117d0a58e78ead07cc40708796487808c33
Benedek Vartok <vartokb@student.ethz.ch>
2013-03-28 16:13:52 +0100

    - wrote tests for iframe and info bar contents - changed assertion style from 'should' to 'expect'

--- test/unittest.js
+++ test/unittest.js
@@ -1,9 +1,47 @@
 function doUnitTest() {
 	describe('iframe', function () {
 		var iframe = document.getElementById('mainframe');
+		var documentElement;
+		var body;
 
-		it('is in the DOM', function () {
-			should.exist(iframe);
+		it('should be in the DOM', function () {
+			expect(iframe).to.exist;
 		});
+
+		it('should have the correct initial content', function () {
+			documentElement = (iframe.contentDocument ? iframe.contentDocument
+				: iframe.contentWindow.document).documentElement;
+
+			expect(documentElement).to.exist;
+
+			body = documentElement.getElementsByTagName('body')[0];
+
+			expect(body).to.exist;
+			expect(body).to.have.property('children').with.length.of.at.least(2);
+
+			expect(body.children[0].tagName).to.equal('H1');
+			expect(body.children[0].innerHTML.trim()).to.equal(
+				'Good! We are about to start...');
+
+			expect(body.children[1].tagName).to.equal('DIV');
+			expect(body.children[1].innerHTML.trim()).to.equal(
+				'The game has not started yet. We are waiting for all the ' +
+				'players to connect.');
+		});
+	});
+
+	describe('info bar', function () {
+		it('should be in the DOM', function() {
+			expect(document.getElementById('gn_header')).to.exist;
+		});
+
+		it('should contain 3 boxes', function() {
+			expect(document.getElementById('visualstate_fieldset')).to.exist;
+			expect(document.getElementById('visualtimer_fieldset')).to.exist;
+			expect(document.getElementById('statedisplay_fieldset')).to.exist;
+		});
+	});
+
+	describe('caching', function () {
 	});
 };
