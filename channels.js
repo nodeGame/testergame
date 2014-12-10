ccc844dd28881e4117008022817052256cf29fbc
Benedek Vartok <vartokb@student.ethz.ch>
2014-12-10 17:35:24 +0100

    Stripped down completely and updated

---
+++ channels.js
@@ -0,0 +1,12 @@
+/**
+ * # Channels definition file for Tester Game
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ */
+module.exports = [
+    {
+        name: 'testergame',
+        admin: 'testergame/admin',
+        player: 'testergame'
+    }
+];
---
+++ server/game.settings.js
@@ -0,0 +1,14 @@
+/**
+ * # Game settings: Tester Game
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ */
+
+module.exports = {
+    gamePaths: {
+        logic:    'game.logic.js',
+        player:   'game.player.js'
+    }
+};
