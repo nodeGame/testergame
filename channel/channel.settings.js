b4a055c7d20e8c46886c335393d60ba669ee09fc
Stefano Balietti <futur.dorko@gmail.com>
2015-06-27 15:14:07 +0200

    newgameserver

---
+++ channel/channel.settings.js
@@ -0,0 +1,10 @@
+/**
+ * # Channels definition file for Tester Game
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ */
+module.exports = {
+        
+    adminServer: 'testergame/admin',
+    playerServer: 'testergame',
+};
