8646087c05686081bd00adab08a0265fc5750716
Stefano Balietti <futur.dorko@gmail.com>
2015-06-29 13:44:34 +0200

    disabled auth

---
+++ auth/auth.settings.js
@@ -0,0 +1,12 @@
+/**
+ * # Auth settings
+ * Copyright(c) 2015 Stefano Balietti
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+module.exports = {
+    enabled: false, // [true, false] Default: TRUE.
+};
