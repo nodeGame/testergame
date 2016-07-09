71df2c3b19b8bcf527bd12e55db9873b189cfc23
Stefano Balietti <futur.dorko@gmail.com>
2016-07-09 00:13:54 -0400

    reverting

--- game/game.settings.js
+++ game/game.settings.js
@@ -1,14 +1,12 @@
 /**
  * # Game settings: Tester Game
- * Copyright(c) 2014 Stefano Balietti
+ * Copyright(c) 2016 Stefano Balietti
  * MIT Licensed
  *
  * http://www.nodegame.org
  */
 
 module.exports = {
-    gamePaths: {
-        logic:    'game.empty.js',
-        player:   'game.empty.js'
-    }
+
+    foo: "bar"
 };
